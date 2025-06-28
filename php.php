6e27d195a733889075a2f844f0ee8548eaa4683a
florbiro <dra.biro.florencia@gmail.com>
2025-06-28 20:19:27 -0300

    111

---
+++ php.php
@@ -0,0 +1,30 @@
+git : dra. / p90P
+
+
+echo "# florbiro.github.io" >> README.md
+git init
+git add .
+git commit -m " "
+git branch -M main
+git remote add origin https://github.com/florbiro/florbiro.github.io.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/florbiro/florbiro.github.io.git
+git branch -M main
+git push -u origin main
+
+git push -u origin master
+
+ ******************
+
+ echo "# gineconsulta" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/florbiro/gineconsulta.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/florbiro/gineconsulta.git
+git branch -M main
+git push -u origin main
---
+++ stylesheets/php.php
@@ -0,0 +1,25 @@
+  
+  
+  
+  
+  
+  
+  
+  
+  color: rgb(245, 245, 202);
+  background-color:rgb(236, 224, 207);
+
+
+checkdate
+responsive 
+target blank 
+alternativos
+
+  ideas :
+  tarjeta virtual 
+  respuestas automaticas 
+
+
+
+
+  Botón de arrepentimiento
