452eb6ea4925d79ce735eb179d91c51a604a7805
florbiro <dra.biro.florencia@gmail.com>
2025-06-29 17:17:11 -0300

    vacunacion

--- php.php
+++ php.php
@@ -42,3 +42,15 @@ git push -u origin main
 git remote add origin https://github.com/florbiro/ginecojunio.git
 git branch -M main
 git push -u origin main
+
+echo "# gineconsulta" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/florbiro/gineconsulta.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/florbiro/gineconsulta.git
+git branch -M main
+git push -u origin main
